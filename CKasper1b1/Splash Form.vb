e39990ec43fc5ddfe8785a0034e94f7f6b63bd25
corey <corey@DESKTOP-TA04OBE>
2016-08-25 18:04:11 -0500

    Add project files.

---
+++ CKasper1b1/Splash Form.vb
@@ -0,0 +1,17 @@
+﻿Public Class frmSplash
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub frmSplash_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs)
+        Me.Close()
+    End Sub
+
+    Private Sub tmrExit_Tick(sender As Object, e As EventArgs) Handles tmrExit.Tick
+        Me.Close()
+    End Sub
+End Class
