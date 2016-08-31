76fd015e0f138fe439a944b6f42d77501ca98c00
corey <corey@DESKTOP-TA04OBE>
2016-08-31 18:14:55 -0500

    Added 1D Bakery

---
+++ Ckasper1d1/Main Form.vb
@@ -0,0 +1,31 @@
+﻿' Name  Bakery Project      
+' Purpose   Calcutates total number of item sold
+' Corey Kasper
+
+Public Class frmMain
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtMuffins.TextChanged
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ' Prepare screen for the sale
+        txtDonuts.Text = String.Empty
+        txtMuffins.Text = String.Empty
+        lblTotalItems.Text = String.Empty
+        lblTotalSales.Text = String.Empty
+        ' Send the focus to the Donuts
+        txtDonuts.Focus()
+    End Sub
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        ' Calculate number of items sold and total sales
+        lblTotalItems.Text = Val(txtDonuts.Text) + Val(txtMuffins.Text)
+        lblTotalSales.Text = Val(lblTotalItems.Text) * 0.5
+        lblTotalSales.Text = Format(lblTotalSales.Text, "Currency")
+    End Sub
+
+End Class
