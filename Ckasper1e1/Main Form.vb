b50c39b45ece61451bb973105e1f802461cc30b1
corey <corey@DESKTOP-TA04OBE>
2016-09-07 19:33:56 -0500

    Added 1e1

---
+++ Ckasper1e1/Main Form.vb
@@ -0,0 +1,15 @@
+﻿Public Class frmMain
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        ' Calculates assassed and property *0.5
+        txtprob.Text = Val(txtassessed.Text) * 0.5
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtassessed.Text = String.Empty
+        txtprob.Text = String.Empty
+    End Sub
+End Class
