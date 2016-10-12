17de7b0738018c7697b0f00b344e8577d0303f13
corey <corey@DESKTOP-TA04OBE>
2016-10-12 17:03:10 -0500

    Added 2-1

---
+++ Ckasper2d1/Form1.Designer.vb
@@ -0,0 +1,459 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnConvertEqual = New System.Windows.Forms.Button()
+        Me.btnConvertGreaterLess = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblEuro = New System.Windows.Forms.Label()
+        Me.lblBritpound = New System.Windows.Forms.Label()
+        Me.lblSouthrand = New System.Windows.Forms.Label()
+        Me.txtAmericanDollar = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.lblTotalDue = New System.Windows.Forms.Label()
+        Me.txtPounds = New System.Windows.Forms.TextBox()
+        Me.txtPriceperlb = New System.Windows.Forms.TextBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.btnCalc4 = New System.Windows.Forms.Button()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.lblMonthlyBill = New System.Windows.Forms.Label()
+        Me.txtPrevious = New System.Windows.Forms.TextBox()
+        Me.txtCurrent = New System.Windows.Forms.TextBox()
+        Me.btnCalcdiv = New System.Windows.Forms.Button()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.Label17 = New System.Windows.Forms.Label()
+        Me.lblQuotient = New System.Windows.Forms.Label()
+        Me.txtSecond = New System.Windows.Forms.TextBox()
+        Me.txtFrist = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnConvertEqual
+        '
+        Me.btnConvertEqual.Location = New System.Drawing.Point(144, 40)
+        Me.btnConvertEqual.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnConvertEqual.Name = "btnConvertEqual"
+        Me.btnConvertEqual.Size = New System.Drawing.Size(118, 39)
+        Me.btnConvertEqual.TabIndex = 0
+        Me.btnConvertEqual.Text = "Convert ="
+        Me.btnConvertEqual.UseVisualStyleBackColor = True
+        '
+        'btnConvertGreaterLess
+        '
+        Me.btnConvertGreaterLess.Location = New System.Drawing.Point(144, 89)
+        Me.btnConvertGreaterLess.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnConvertGreaterLess.Name = "btnConvertGreaterLess"
+        Me.btnConvertGreaterLess.Size = New System.Drawing.Size(118, 39)
+        Me.btnConvertGreaterLess.TabIndex = 1
+        Me.btnConvertGreaterLess.Text = "Convert <>"
+        Me.btnConvertGreaterLess.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 40)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(117, 20)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "American &Dollar"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(269, 41)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 20)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "&Euro"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(385, 40)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(97, 20)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "British &pound"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(501, 41)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(132, 20)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "South African &rand"
+        '
+        'lblEuro
+        '
+        Me.lblEuro.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblEuro.Location = New System.Drawing.Point(269, 61)
+        Me.lblEuro.Name = "lblEuro"
+        Me.lblEuro.Size = New System.Drawing.Size(110, 36)
+        Me.lblEuro.TabIndex = 6
+        '
+        'lblBritpound
+        '
+        Me.lblBritpound.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblBritpound.Location = New System.Drawing.Point(385, 61)
+        Me.lblBritpound.Name = "lblBritpound"
+        Me.lblBritpound.Size = New System.Drawing.Size(110, 36)
+        Me.lblBritpound.TabIndex = 7
+        '
+        'lblSouthrand
+        '
+        Me.lblSouthrand.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblSouthrand.Location = New System.Drawing.Point(501, 61)
+        Me.lblSouthrand.Name = "lblSouthrand"
+        Me.lblSouthrand.Size = New System.Drawing.Size(110, 36)
+        Me.lblSouthrand.TabIndex = 8
+        '
+        'txtAmericanDollar
+        '
+        Me.txtAmericanDollar.Location = New System.Drawing.Point(19, 63)
+        Me.txtAmericanDollar.Name = "txtAmericanDollar"
+        Me.txtAmericanDollar.Size = New System.Drawing.Size(110, 27)
+        Me.txtAmericanDollar.TabIndex = 9
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(32, 13)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(154, 13)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "P. 231 #1 Currency Converter"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(32, 148)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(98, 13)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "P. 231 #3 Tea Time"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(31, 176)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(59, 20)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "Pounds:"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(151, 176)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(63, 20)
+        Me.Label8.TabIndex = 13
+        Me.Label8.Text = "Price/lb:"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(385, 176)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(74, 20)
+        Me.Label9.TabIndex = 14
+        Me.Label9.Text = "Total due:"
+        '
+        'lblTotalDue
+        '
+        Me.lblTotalDue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTotalDue.Location = New System.Drawing.Point(389, 197)
+        Me.lblTotalDue.Name = "lblTotalDue"
+        Me.lblTotalDue.Size = New System.Drawing.Size(110, 27)
+        Me.lblTotalDue.TabIndex = 15
+        '
+        'txtPounds
+        '
+        Me.txtPounds.Location = New System.Drawing.Point(35, 199)
+        Me.txtPounds.Name = "txtPounds"
+        Me.txtPounds.Size = New System.Drawing.Size(110, 27)
+        Me.txtPounds.TabIndex = 16
+        '
+        'txtPriceperlb
+        '
+        Me.txtPriceperlb.Location = New System.Drawing.Point(153, 199)
+        Me.txtPriceperlb.Name = "txtPriceperlb"
+        Me.txtPriceperlb.Size = New System.Drawing.Size(110, 27)
+        Me.txtPriceperlb.TabIndex = 17
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(273, 197)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(109, 27)
+        Me.btnCalculate.TabIndex = 18
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(32, 266)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(207, 13)
+        Me.Label10.TabIndex = 19
+        Me.Label10.Text = "P.232 #4 Tri County Electric w/valdation"
+        '
+        'btnCalc4
+        '
+        Me.btnCalc4.Location = New System.Drawing.Point(273, 314)
+        Me.btnCalc4.Name = "btnCalc4"
+        Me.btnCalc4.Size = New System.Drawing.Size(109, 27)
+        Me.btnCalc4.TabIndex = 20
+        Me.btnCalc4.Text = "Calculate"
+        Me.btnCalc4.UseVisualStyleBackColor = True
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(35, 291)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(67, 20)
+        Me.Label11.TabIndex = 21
+        Me.Label11.Text = "Previous:"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(154, 291)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(60, 20)
+        Me.Label12.TabIndex = 22
+        Me.Label12.Text = "Current:"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(385, 294)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(91, 20)
+        Me.Label13.TabIndex = 23
+        Me.Label13.Text = "Monthly bill:"
+        '
+        'lblMonthlyBill
+        '
+        Me.lblMonthlyBill.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblMonthlyBill.Location = New System.Drawing.Point(388, 314)
+        Me.lblMonthlyBill.Name = "lblMonthlyBill"
+        Me.lblMonthlyBill.Size = New System.Drawing.Size(110, 27)
+        Me.lblMonthlyBill.TabIndex = 24
+        '
+        'txtPrevious
+        '
+        Me.txtPrevious.Location = New System.Drawing.Point(39, 314)
+        Me.txtPrevious.Name = "txtPrevious"
+        Me.txtPrevious.Size = New System.Drawing.Size(110, 27)
+        Me.txtPrevious.TabIndex = 25
+        '
+        'txtCurrent
+        '
+        Me.txtCurrent.Location = New System.Drawing.Point(155, 314)
+        Me.txtCurrent.Name = "txtCurrent"
+        Me.txtCurrent.Size = New System.Drawing.Size(110, 27)
+        Me.txtCurrent.TabIndex = 26
+        '
+        'btnCalcdiv
+        '
+        Me.btnCalcdiv.Location = New System.Drawing.Point(273, 405)
+        Me.btnCalcdiv.Name = "btnCalcdiv"
+        Me.btnCalcdiv.Size = New System.Drawing.Size(109, 27)
+        Me.btnCalcdiv.TabIndex = 27
+        Me.btnCalcdiv.Text = "Calculate"
+        Me.btnCalcdiv.UseVisualStyleBackColor = True
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label14.Location = New System.Drawing.Point(32, 359)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(131, 13)
+        Me.Label14.TabIndex = 28
+        Me.Label14.Text = "p.232 Divison Calculator"
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(38, 384)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(52, 20)
+        Me.Label15.TabIndex = 29
+        Me.Label15.Text = "Frist #:"
+        '
+        'Label16
+        '
+        Me.Label16.AutoSize = True
+        Me.Label16.Location = New System.Drawing.Point(154, 384)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(74, 20)
+        Me.Label16.TabIndex = 30
+        Me.Label16.Text = "Second #:"
+        '
+        'Label17
+        '
+        Me.Label17.AutoSize = True
+        Me.Label17.Location = New System.Drawing.Point(389, 384)
+        Me.Label17.Name = "Label17"
+        Me.Label17.Size = New System.Drawing.Size(70, 20)
+        Me.Label17.TabIndex = 31
+        Me.Label17.Text = "Quotient:"
+        '
+        'lblQuotient
+        '
+        Me.lblQuotient.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblQuotient.Location = New System.Drawing.Point(388, 407)
+        Me.lblQuotient.Name = "lblQuotient"
+        Me.lblQuotient.Size = New System.Drawing.Size(110, 27)
+        Me.lblQuotient.TabIndex = 32
+        '
+        'txtSecond
+        '
+        Me.txtSecond.Location = New System.Drawing.Point(157, 405)
+        Me.txtSecond.Name = "txtSecond"
+        Me.txtSecond.Size = New System.Drawing.Size(110, 27)
+        Me.txtSecond.TabIndex = 33
+        '
+        'txtFrist
+        '
+        Me.txtFrist.Location = New System.Drawing.Point(42, 405)
+        Me.txtFrist.Name = "txtFrist"
+        Me.txtFrist.Size = New System.Drawing.Size(110, 27)
+        Me.txtFrist.TabIndex = 34
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(543, 431)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(89, 29)
+        Me.Button1.TabIndex = 35
+        Me.Button1.Text = "Exit"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(677, 492)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtFrist)
+        Me.Controls.Add(Me.txtSecond)
+        Me.Controls.Add(Me.lblQuotient)
+        Me.Controls.Add(Me.Label17)
+        Me.Controls.Add(Me.Label16)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.btnCalcdiv)
+        Me.Controls.Add(Me.txtCurrent)
+        Me.Controls.Add(Me.txtPrevious)
+        Me.Controls.Add(Me.lblMonthlyBill)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.btnCalc4)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.txtPriceperlb)
+        Me.Controls.Add(Me.txtPounds)
+        Me.Controls.Add(Me.lblTotalDue)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtAmericanDollar)
+        Me.Controls.Add(Me.lblSouthrand)
+        Me.Controls.Add(Me.lblBritpound)
+        Me.Controls.Add(Me.lblEuro)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnConvertGreaterLess)
+        Me.Controls.Add(Me.btnConvertEqual)
+        Me.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "Form1"
+        Me.Text = "Ckasper If-Else statements"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnConvertEqual As Button
+    Friend WithEvents btnConvertGreaterLess As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lblEuro As Label
+    Friend WithEvents lblBritpound As Label
+    Friend WithEvents lblSouthrand As Label
+    Friend WithEvents txtAmericanDollar As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents lblTotalDue As Label
+    Friend WithEvents txtPounds As TextBox
+    Friend WithEvents txtPriceperlb As TextBox
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents Label10 As Label
+    Friend WithEvents btnCalc4 As Button
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents lblMonthlyBill As Label
+    Friend WithEvents txtPrevious As TextBox
+    Friend WithEvents txtCurrent As TextBox
+    Friend WithEvents btnCalcdiv As Button
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label16 As Label
+    Friend WithEvents Label17 As Label
+    Friend WithEvents lblQuotient As Label
+    Friend WithEvents txtSecond As TextBox
+    Friend WithEvents txtFrist As TextBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Ckasper2d1/Form1.vb
@@ -0,0 +1,138 @@
+﻿'Name:          Money Conversion
+'Purpose:       Test Data Types
+'Programmer:    Corey Kasper 10/12/2016
+
+Option Strict On
+Option Explicit On
+Option Infer Off
+
+Public Class Form1
+    Private Sub btnConvertEqual_Click(sender As Object, e As EventArgs) Handles btnConvertEqual.Click
+
+        Const dblEruo_rate As Double = 0.9
+        Const dblPound_rate As Double = 0.79
+        Const dblRand_rate As Double = 13.89
+        Dim dblAmDollar As Double
+        Dim dblEuro As Double
+        Dim dblPound As Double
+        Dim dblRand As Double
+
+        lblBritpound.Text = String.Empty
+        lblEuro.Text = String.Empty
+        lblSouthrand.Text = String.Empty
+
+        If txtAmericanDollar.Text = String.Empty Then
+            MessageBox.Show("Please enter number of American dollars.", "Currency Converter", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+            Double.TryParse(txtAmericanDollar.Text, dblAmDollar)
+
+            dblEuro = dblAmDollar * dblEruo_rate
+            dblPound = dblAmDollar * dblPound_rate
+            dblRand = dblAmDollar * dblRand_rate
+
+            lblEuro.Text = dblEuro.ToString("C2")
+            lblBritpound.Text = dblPound.ToString("C2")
+            lblSouthrand.Text = dblRand.ToString("C2")
+        End If
+
+
+    End Sub
+
+    Private Sub btnConvertGreaterLess_Click(sender As Object, e As EventArgs) Handles btnConvertGreaterLess.Click
+
+        Const dblEruo_rate As Double = 0.9
+        Const dblPound_rate As Double = 0.79
+        Const dblRand_rate As Double = 13.89
+        Dim dblAmDollar As Double
+        Dim dblEuro As Double
+        Dim dblPound As Double
+        Dim dblRand As Double
+
+        lblBritpound.Text = String.Empty
+        lblEuro.Text = String.Empty
+        lblSouthrand.Text = String.Empty
+
+        If txtAmericanDollar.Text <> String.Empty Then
+            Double.TryParse(txtAmericanDollar.Text, dblAmDollar)
+
+            dblEuro = dblAmDollar * dblEruo_rate
+            dblPound = dblAmDollar * dblPound_rate
+            dblRand = dblAmDollar * dblRand_rate
+
+            lblEuro.Text = dblEuro.ToString()
+            lblBritpound.Text = dblPound.ToString("C2")
+            lblSouthrand.Text = dblRand.ToString("C2")
+            'MessageBox.Show("Please enter number of American dollars.", "Currency Converter", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+            MessageBox.Show("Please enter number of American dollars.", "Currency Converter", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim dblTotalDue As Double
+        Dim dblPounds As Double
+        Dim dblPricetoLB As Double
+        Const dblShipping As Double = 15
+        Dim Msg As DialogResult = MessageBox.Show("Charge for shipping?", "Shipping", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+        lblTotalDue.Text = String.Empty
+
+        Double.TryParse(txtPounds.Text, dblPounds)
+        Double.TryParse(txtPriceperlb.Text, dblPricetoLB)
+
+
+        'MessageBox.Show("Charge for shipping?", "Shipping", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If Msg = DialogResult.Yes Then
+            dblTotalDue = dblPounds * dblPricetoLB + dblShipping
+            lblTotalDue.Text = dblTotalDue.ToString("C2")
+
+        Else
+            dblTotalDue = dblPounds * dblPricetoLB
+            lblTotalDue.Text = dblTotalDue.ToString("C2")
+
+        End If
+
+
+    End Sub
+
+    Private Sub btnCalc4_Click(sender As Object, e As EventArgs) Handles btnCalc4.Click
+        'current - Previous * .13
+        Dim DblCurrent As Double
+        Dim DblPrevious As Double
+        Dim DblMonthlybill As Double
+
+        Double.TryParse(txtCurrent.Text, DblCurrent)
+        Double.TryParse(txtPrevious.Text, DblPrevious)
+
+        If txtCurrent.Text = String.Empty Or txtPrevious.Text = String.Empty Or DblCurrent > DblPrevious Then
+            MessageBox.Show("Current needs to be lower than Previous.", "Triple County", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            DblMonthlybill = (DblPrevious - DblCurrent) * 0.13
+            lblMonthlyBill.Text = DblMonthlybill.ToString("C2")
+        End If
+    End Sub
+
+    Private Sub btnCalcdiv_Click(sender As Object, e As EventArgs) Handles btnCalcdiv.Click
+        Dim dblFrist As Double
+        Dim dblSecond As Double
+        Dim dblQuotient As Double
+
+        Double.TryParse(txtFrist.Text, dblFrist)
+        Double.TryParse(txtSecond.Text, dblSecond)
+
+        If txtFrist.Text = "0" Or txtSecond.Text = "0" Or txtFrist.Text = String.Empty Or txtSecond.Text = String.Empty Then
+            MessageBox.Show("Can not use 0", "Calculator", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        ElseIf dblFrist > dblSecond Then
+            dblQuotient = dblFrist / dblSecond
+            lblQuotient.Text = dblQuotient.ToString()
+        ElseIf dblSecond > dblFrist Then
+            dblQuotient = dblSecond / dblFrist
+            lblQuotient.Text = dblQuotient.ToString()
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+
+    End Sub
+End Class
